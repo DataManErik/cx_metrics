7a8c89f01b3d5d94f7e294096b15ca78814b641b
deepturkey <95242641+deepturkey@users.noreply.github.com>
2024-06-18 07:38:04 -0600

    Update cx_metrics reply_time_query.sql
    Formatting and comment updates

--- cx_metrics reply_time_query.sql
+++ cx_metrics reply_time_query.sql
@@ -1,32 +1,39 @@
+#This @rowindex variable will be used to order reply_time values and find the median.
+	
 SET @rowindex := -1;
 
 #This "prep" CTE will convert the table's datatypes so the columns can be aggregated later. 
 #reply_time and full_resolution_time are in minutes, the query divides these by 60 to convert to hours.
+#SUBSTRING_INDEX is used to remove errant characters from the agent and manager dimensions.	
 #The WHERE clause filters out a single row of null values.
+	
 WITH prep AS 
 (
 SELECT
 	CAST(reply_time/60 AS DECIMAL(10,2)) AS reply_time, #Rounding to 1 decimal resulted in zero values for some records
 	CAST(full_resolution_time/60 AS DECIMAL(10,2)) AS full_resolution_time, #Rounding to 1 decimal resulted in zero values for some records
-    CAST(num_replies AS SIGNED) AS num_replies, #MySQL requires either 'SIGNED' or 'UNSIGNED' for the CAST function to produce an integer.
+    	CAST(num_replies AS SIGNED) AS num_replies, #MySQL requires either 'SIGNED' or 'UNSIGNED' for the CAST function to produce an integer.
 	CAST(is_rerouted AS SIGNED) AS is_rerouted,
-    CAST(is_issue_solved AS SIGNED) AS is_issue_solved,
+    	CAST(is_issue_solved AS SIGNED) AS is_issue_solved,
 	CAST(is_promoter AS SIGNED) AS is_promoter,
 	CAST(group_number AS SIGNED) AS group_number,
-    SUBSTRING_INDEX(agent, 'Ê', 1) AS agent,
-    SUBSTRING_INDEX(manager,'Ê', 1) AS manager
+    	SUBSTRING_INDEX(agent, 'Ê', 1) AS agent,
+    	SUBSTRING_INDEX(manager,'Ê', 1) AS manager
 FROM cx_metrics_2
 WHERE id IS NOT NULL
 )
 
 #This query defines logic to find all relevant "reply_time" dimensions.
+	
 SELECT 
-    ROUND(AVG(prep.reply_time),2) AS avg_reply_time,
-    (#This subquery defines the logic needed to find "median_reply_time."
+	ROUND(AVG(prep.reply_time),2) AS avg_reply_time,
+	(
+	#This subquery defines the logic needed to find "median_reply_time."
 		SELECT
 		ROUND(AVG(reply_med_prep.reply_time),2)
 		FROM
 		(
+		#This subquery orders reply_time from smallest to largest, assigns a rowindex inreasing by 1 to each row.
 			SELECT
 			@rowindex := @rowindex + 1 AS rowindex,
 			reply_time
@@ -34,21 +41,23 @@ SELECT
 			ORDER BY reply_time
 		) as reply_med_prep
 		WHERE
-		reply_med_prep.rowindex IN (FLOOR(@rowindex / 2) , CEIL(@rowindex / 2))
+		reply_med_prep.rowindex IN (FLOOR(@rowindex / 2) , CEIL(@rowindex / 2)) #This logic is finding the median using the rowindex.
 	) AS median_reply_time,
-    MIN(prep.reply_time) AS min_reply_time,
-    MAX(prep.reply_time) AS max_reply_time,
-    (#This subquery counts the number of records where both "reply_time" and "full_resolution_time" are 0.
+	MIN(prep.reply_time) AS min_reply_time,
+	MAX(prep.reply_time) AS max_reply_time,
+	(
+	#This subquery counts the number of records where both "reply_time" and "full_resolution_time" are 0.
 		SELECT COUNT(*) 
 		FROM prep 
-        WHERE prep.reply_time =  0
-        AND full_resolution_time = 0
+        	WHERE prep.reply_time =  0
+        	AND full_resolution_time = 0
 	) AS count_zero_both_time,
-	(#This subquery counts the number of records where "reply_time" is 0 but "full_resolution_time" is > 0.
+	(
+	#This subquery counts the number of records where "reply_time" is 0 but "full_resolution_time" is > 0.
 		SELECT COUNT(*) 
 		FROM prep 
-        WHERE prep.reply_time =  0
-        AND full_resolution_time > 0
+		WHERE prep.reply_time =  0
+		AND full_resolution_time > 0
 	) AS count_zero_reply_but_some_resolution_time,
 	COUNT(*) AS count_records
 FROM prep;
